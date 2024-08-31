2b4c30e5bc6a14f72ad3d4747979abf704c4d464
JulianAhumada86 <julianahumada86@gmail.com>
2024-08-30 23:46:21 -0300

    video

--- BusquedaDelTesoro/script.js
+++ BusquedaDelTesoro/script.js
@@ -1,15 +1,20 @@
 document.getElementById('checkButton').addEventListener('click', function() {
     const userInput = document.getElementById('userInput').value;
     const correctAnswer = document.querySelector('.input-section').dataset.correctAnswer;
+    const resultMessage = document.getElementById('resultMessage');
 
     if (userInput === correctAnswer) {
-        document.getElementById('resultMessage').innerText = "El mensaje es correcto, tu siguiente pista es:";
-        document.getElementById('userInput').style.display = 'none';
-        document.getElementById('checkButton').style.display = 'none';
-
         // Muestra la pista oculta
         document.getElementById('hiddenHint').style.display = 'block';
+
+        // Oculta el contenedor de reto y la sección de entrada
+        document.getElementById('retoContainer').style.display = 'none';
+        document.getElementById('inputSection').style.display = 'none';
+
+        // Limpia el mensaje de resultado
+        resultMessage.innerText = "Bien! A seguir adelante";
     } else {
-        document.getElementById('resultMessage').innerText = "Respuesta incorrecta. Intenta nuevamente.";
+        // Muestra el mensaje de respuesta incorrecta
+        resultMessage.innerText = "Respuesta incorrecta. Intenta nuevamente.";
     }
 });
